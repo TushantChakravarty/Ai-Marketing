f00520b8b352d8f920fa0b31353385e01a28bbe7
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2026-05-07 11:19:48 +0530

    updates

--- apps/mobile/src/config/api.config.ts
+++ apps/mobile/src/config/api.config.ts
@@ -12,3 +12,12 @@ const API_CONFIG = {
 };
 
 export default API_CONFIG;
+
+/*
+  Note: The BASE_URL is set to the same value for both development and production in this example. 
+  In a real-world scenario, you would typically have different URLs for development and production environments. 
+  You can adjust the BASE_URL values as needed when deploying to production.
+    BASE_URL: __DEV__
+    ? 'http://192.168.29.218:3001/api/v1'
+    : 'http://192.168.29.218:3001/api/v1',
+*/
